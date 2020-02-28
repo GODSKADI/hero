9f271c9b3b6312ba515bc83c2987ed4a01b2304c
godskadi <aidrean.gf@gmail.com>
2020-02-28 19:51:19 +0100

    hero

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
-// Conectando y seleccionado la base de datos  
-$dbconn = pg_connect('DATABASE_URL')
+// Conectando y seleccionado la base de datos 
+ $dbopts = getenv('DATABASE_URL');
+$dbconn = pg_connect($dbopts)
     or die('No se ha podido conectar: ' . pg_last_error());
 
 // Realizando una consulta SQL
